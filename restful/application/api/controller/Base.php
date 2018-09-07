30ee1acf8cb5b64293a0ea7402c99056f710048d
xly1993 <38456634+xly1993@users.noreply.github.com>
2018-09-07 11:04:54 +0800

    新增AES加密
    数据加密

--- restful/application/api/controller/Base.php
+++ restful/application/api/controller/Base.php
@@ -234,5 +234,58 @@ class Base extends Controller
    }
 
 
+    /**
+     * var string $method 加解密方法，可通过openssl_get_cipher_methods()获得
+     */
+    protected $method = 'AES-128-ECB';
+
+    /**
+     * var string $secret_key 加解密的密钥
+     */
+    protected $secret_key = 'ceshi';
+
+    /**
+     * var string $iv 加解密的向量，有些方法需要设置比如CBC
+     */
+    protected $iv = '';
+
+    /**
+     * var string $options （不知道怎么解释，目前设置为0没什么问题）
+     */
+    protected $options = 0;
+    /**
+     * 加密方法，对数据进行加密，返回加密后的数据
+     *
+     * @param string $data 要加密的数据
+     *
+     * @return string
+     *
+     */
+    public function encrypt($data)
+    {
+        return openssl_encrypt($data, $this->method, $this->secret_key, $this->options, $this->iv);
+    }
+
+    /**
+     * 解密方法，对数据进行解密，返回解密后的数据
+     *
+     * @param string $data 要解密的数据
+     *
+     * @return string
+     *
+     */
+    public function decrypt($data)
+    {
+        return openssl_decrypt($data, $this->method, $this->secret_key, $this->options, $this->iv);
+    }
+
+
+
+
+
+
+
+
+
 
 }
